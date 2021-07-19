4eab000890671bdee6c3b76ba140b43cdfcd2227
rivels <srivel9312@gmail.com>
2021-07-19 18:26:47 +0900

    modified init.py

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, jsonify, request
 import pymysql
 pymysql.install_as_MySQLdb()
 import MySQLdb
@@ -5,19 +6,77 @@ import MySQLdb
 from flask import *
 from app.config import Config
 
+# database에 접근
+db= pymysql.connect(host='35.223.152.188',
+                     port=3306,
+                     user='root',
+                     passwd='dss',
+                     charset='utf8')
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-from app.items.mysql_zigbang import Zigbang, ZigbangApt, zigbang_db, zigbang_apt_db
-from app.items.mysql_kakao import KaKaoApt, KaKaoOneroom, KaKaoOfficetel, KaKaoVilla
-from app.items.mysql_kakao import kakao_apt_db, kakao_oneroom_db, kakao_officetel_db, kakao_villa_db
+# from app.items.mysql_zigbang import Zigbang, ZigbangApt, zigbang_db, zigbang_apt_db
+# from app.items.mysql_kakao import KaKaoApt, KaKaoOneroom, KaKaoOfficetel, KaKaoVilla
+# from app.items.mysql_kakao import kakao_apt_db, kakao_oneroom_db, kakao_officetel_db, kakao_villa_db
 
 
-@app.route("/home")
+@app.route("/")
 def index():
-    return render_template("index3.html")
+    return render_template("map.html")
+
+@app.route('/apt_listup', methods=['GET'])
+def apt_listup():
+    cursor= db.cursor()
+
+    sql_ls = ['use data','select name, dong, lat, lng, image, rent_min, rent_max, rent_avg, sales_min, sales_max, sales_avg from zigbang_apt;']
+
+    for sql in sql_ls:
+        cursor.execute(sql)
+    
+    result= cursor.fetchall()
+    
+    return jsonify({'result': 'success', 'apt_list': result})
+
+@app.route('/villa_listup', methods=['GET'])
+def villa_listup():
+    cursor= db.cursor()
+
+    sql_ls = ['use data','select address, lat, lng, images_thumbnail, deposit, rent from zigbang_villa;']
+
+    for sql in sql_ls:
+        cursor.execute(sql)
+    
+    result= cursor.fetchall()
+    
+    return jsonify({'result': 'success', 'villa_list': result})
+
+@app.route('/oneroom_listup', methods=['GET'])
+def oneroom_listup():
+    cursor= db.cursor()
+
+    sql_ls = ['use data','select address, lat, lng, images_thumbnail, deposit, rent, sales_title from zigbang_oneroom;']
+
+    for sql in sql_ls:
+        cursor.execute(sql)
+    
+    result= cursor.fetchall()
+    
+    return jsonify({'result': 'success', 'oneroom_list': result})
+
+@app.route('/officetel_listup', methods=['GET'])
+def officetel_listup():
+    cursor= db.cursor()
+
+    sql_ls = ['use data','select address, lat, lng, images_thumbnail, deposit, rent, sales_title from zigbang_officetel;']
+
+    for sql in sql_ls:
+        cursor.execute(sql)
+    
+    result= cursor.fetchall()
+    
+    return jsonify({'result': 'success', 'officetel_list': result})
 
 
 @app.route("/api")
@@ -40,14 +99,9 @@ def show_map():
     print(result)
 
     return jsonify(result)
-    # return render_template("test.html", data=result)
+    return render_template("test.html", data=result)
 
 
-# @app.route("/home/map")
-# def test():
-#     return render_template("test.html")
-
-
-
-
-
+@app.route("/home/map")
+def test():
+    return render_template("test.html")
